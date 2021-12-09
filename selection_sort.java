3b3eb667fd4de80fc82dc67b479407455b7e78df
abhishek choudhry <66086502+choudhary805@users.noreply.github.com>
2021-12-09 19:00:06 +0530

    Update selection_sort.java

--- selection_sort.java
+++ selection_sort.java
@@ -1,25 +1,33 @@
-import java.util.*;
+import java.util.Scanner;
 public class selection_sort {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("abhi");
-        int n=6,a=0;
-        int[] arr = new int[n];
-        for(int i=0;i<n;i++)
-            arr[i]=sc.nextInt();
-    while(a<n){
-        int min=arr[a],min_index=a;
-    for(int i=a+1;i<n;i++){
-        if(arr[i]<min){
-            min=arr[i];
-            min_index=i;
+    void sort(int arr[]){
+        int n=arr.length;
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                if(arr[j]<arr[i]){
+                    int temp=arr[j];
+                    arr[j]=arr[i];
+                    arr[i]=temp;
+                }
+            }
         }
-        int temp=arr[0];
-        arr[0]=min;
-        arr[min_index]=temp;
-    }}a++;
-    System.out.println("Sorted array is : ");
-    for(int i=0;i<n;i++)
-        System.out.println(arr[i]);
+    }
+    void print(int arr[]){
+        int n = arr.length;
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+ " ");
+        }System.out.println();
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int[] arr=new int[6];
+        System.out.println("Enter array elements :");
+        for(int i=0;i<6;i++) {
+            arr[i]=sc.nextInt();
+        }
+        selection_sort ob = new selection_sort();
+        ob.print(arr);
+        ob.sort(arr);
+        ob.print(arr);
 }}
 
